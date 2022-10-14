96e469b1447f1890cef6c9e8bef396726e694ad3
TatianaChernenko <ezhik.live@gmail.com>
2022-10-14 14:25:40 +0400

    adding code to popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,14 @@
+const modalMain = document.getElementById('modal_main');
+const modalSuccess = document.getElementById('modal_success');
+
+const modalClose = document.getElementsByClassName('modal__close');
+const showSuccess = document.getElementsByClassName('show-success');
+
+modalMain.className = 'modal modal_active';
+
+for (let i = 0; i < modalClose.length; i++)
+modalClose[i].onclick = () => {
+    modalClose[i].closest('.modal').className = 'modal';
+    if( modalClose[i] === showSuccess[0])
+    modalSuccess.className = 'modal modal_active'
+}
